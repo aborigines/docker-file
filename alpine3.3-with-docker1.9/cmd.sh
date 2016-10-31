b63c7015f5ce8817fcb67732f946cfb36b2d144e
Watcharapol Tadtiang <7aborigines7@gmail.com>
2016-10-31 15:36:25 +0700

    update
    Signed-off-by: Watcharapol Tadtiang <7aborigines7@gmail.com>

--- alpine3.3-with-docker1.9/cmd.sh
+++ alpine3.3-with-docker1.9/cmd.sh
@@ -7,4 +7,4 @@ docker build --no-cache -t alpine:docker-1.9 -f Dockerfile .
 docker rm -f $(docker ps -aq --filter="name=$name")
 
 # -v /var/run/docker1.sock:/var/run/docker1.sock
-docker run -it -d --name $name -v $HOME/Source:/data alpine:docker-1.9
+docker run --privileged -it -d --name $name -v $HOME/Source:/data alpine:docker-1.9
